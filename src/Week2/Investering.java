ed52e461846a907a429f5578f65936d1c7e374f6
Tiago2526 <tiagonizferreira@gmail.com>
2025-09-24 13:24:06 +0200

    week 1

--- src/Week2/Investering.java
+++ src/Week2/Investering.java
@@ -1,7 +1,25 @@
 package Week2;
 
+import java.util.Scanner;
+
 public class Investering {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int hoofdsom, jaarlijkseRent, hoelang;
+
+        System.out.print("Wat is de initiële investering: ");
+        hoofdsom = sc.nextInt();
+
+        System.out.print("Het jaarlijkse rentpercentage: ");
+        jaarlijkseRent = sc.nextInt();
+
+        System.out.print("Aantal jaren dat het geld wordt vastgehouden: ");
+        hoelang = sc.nextInt();
+
+        int eindsaldo = hoofdsom * (1 + jaarlijkseRent/100)^hoelang;
+        System.out.println("Eindesaldo = " + eindsaldo);
+
+
 
     }
 }
