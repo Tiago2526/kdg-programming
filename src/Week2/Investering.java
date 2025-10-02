3e6b8e5c05f3522f19d2169efb845fd9c35d3a90
Tiago2526 <tiagonizferreira@gmail.com>
2025-10-02 11:57:19 +0200

    oef 1 week 2

--- src/Week2/Investering.java
+++ src/Week2/Investering.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class Investering {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int hoofdsom, jaarlijkseRent, hoelang;
+        double hoofdsom, jaarlijkseRent;
+        int hoelang;
 
         System.out.print("Wat is de initiële investering: ");
         hoofdsom = sc.nextInt();
@@ -16,10 +17,11 @@ public class Investering {
         System.out.print("Aantal jaren dat het geld wordt vastgehouden: ");
         hoelang = sc.nextInt();
 
-        int eindsaldo = hoofdsom * (1 + jaarlijkseRent/100)^hoelang;
-        System.out.println("Eindesaldo = " + eindsaldo);
+        System.out.print("Welke valuta gebruik je? ");
+        sc.nextLine();
+        String valuta = sc.nextLine();
 
-
-
+        double eindsaldo = hoofdsom * Math.pow((1 + jaarlijkseRent/100), hoelang);
+        System.out.println("Eindesaldo = " + valuta + " " + eindsaldo);
     }
 }
