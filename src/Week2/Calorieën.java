60f815b2e6324c5c94c7a60b2a4cb89795b3d93a
Tiago2526 <tiagonizferreira@gmail.com>
2025-10-05 16:37:06 +0200

    oef 3 week 2

---
+++ src/Week2/Calorieën.java
@@ -0,0 +1,21 @@
+package Week2;
+
+import java.util.Scanner;
+
+public class Calorieën {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        final int MINSTAANTALMENSEN = 7;
+        final int GEMIDDELDE_CALORIEEN = 2256;
+        String land;
+        int aantalMensen;
+
+        System.out.print("Geef de naam van het land: ");
+        land = sc.nextLine();
+
+        System.out.printf("Hoelveel mensen wonen er in %s? ",land);
+
+
+    }
+}
